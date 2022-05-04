c7224f742f04969de63e2d26bbe2215a28d208a6
Sangeeta Shetty <57719121+Sangeetaaaa@users.noreply.github.com>
2022-05-04 14:58:07 +0530

    Final Upload

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary");
 const multer = require("multer");
+const passport = require("passport");
 const https = require("https");
 const fetch = require('node-fetch');
-const fastcsv = require("fast-csv");
-const fs = require("fs");
+const { UserInfo } = require("git");
 
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear()
 
 
 
@@ -22,15 +24,11 @@ app.use(express.static("public"));
 
 
 
-
-
-
-
 // connecting to mongoose and creating a new dataabase
 mongoose.connect("mongodb://admin-sangeeta:JLlCixxIoO3lYCwJ@cluster0-shard-00-00.vllwc.mongodb.net:27017,cluster0-shard-00-01.vllwc.mongodb.net:27017,cluster0-shard-00-02.vllwc.mongodb.net:27017/spaceDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",  { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 
 
-// creating a schema
+// creating a schema for blogdb
 const cardSchema = new mongoose.Schema ({
   title: String,
   content: String,
@@ -41,8 +39,12 @@ const cardSchema = new mongoose.Schema ({
 const Card = new mongoose.model("Card", cardSchema);
 
 
-
-
+//creating schema for user db
+const userSchema = new mongoose.Schema ({
+  username: String,
+  email: String
+})
+const User = new mongoose.model("User", userSchema);
 
 
 
@@ -66,6 +68,7 @@ app.get("/apod", (req, res) => {
     
     response.on("data", function(data) {
       let nasaAPOD = JSON.parse(data)
+      console.log(nasaAPOD);
       res.render("apod", {nasaAPOD: nasaAPOD})
     })
   })
@@ -96,19 +99,32 @@ app.get("/login", (req, res) => {
   res.render("login")
 })
 
+
+app.get("/AdminLogin", (req, res) => {
+  res.render("AdminLogin")
+})
+
+
 //authentication
-app.post("/login", (req, res) => {
+app.post("/AdminLogin", (req, res) => {
   let username = "admin";
   let password = "space-cosmos123";
 
   // if username and pwd is right then get all the data from the database and forward then to admin page 
   if (req.body.username === username && req.body.password === password) {
     Card.find((err, foundCards) => {
-      !err &&  res.render("admin", {cards: foundCards})
+      User.find((err, foundUsers) => {
+        !err &&  res.render("admin", {cards: foundCards, users: foundUsers})
+      })
     });
   } else {
     res.render("failure")
   }
+})
+
+
+app.get("/userLogin", function (req, res) {
+  res.render('userLogin')
 })
 
 
@@ -157,12 +173,55 @@ app.post("/add" , (req, res) => {
     title: req.body.title,
     source: req.body.source,
     content: req.body.content,
+    date : date,
+    by: "Admin"
   }
+
 
   Card.create(myobj, function(err, response) {
     if (!err) {
       Card.find((err, foundCards) => {
-        !err &&  res.render("admin", {cards: foundCards})
+        User.find((err, foundUsers) => {
+          !err &&  res.render("admin", {cards: foundCards, users: foundUsers})
+        })
+      });
+    }
+  })
+})
+
+//users can add to databaseusing this route
+app.post("/addbyuser" , (req, res) => {
+
+  let myobj = {
+    title: req.body.title,
+    source: req.body.source,
+    content: req.body.content,
+    date : date,
+    by: req.body.username
+  }
+
+  let newuser = {
+    username: req.body.username,
+    email: req.body.email
+  }
+
+  User.findOne({email: newuser.email}, (err, foundUser) => {
+    if(err) {
+      User.create(newuser, (err) => {
+        if(err) console.log(err);
+      })
+    } else {
+      console.log('do nothing');
+    }
+  }) 
+
+  
+
+
+  Card.create(myobj, function(err, response) {
+    if (!err) {
+      Card.find((err, foundCards) => {
+        !err &&  res.redirect("/card")
       });
     }
   })
@@ -173,7 +232,9 @@ app.get("/remove/:item", (req, res) => {
   Card.deleteOne({_id: req.params.item}, function(err) {
     if (!err) {
       Card.find((err, foundCards) => {
-        !err &&  res.render("admin", {cards: foundCards})
+        User.find((err, foundUsers) => {
+          !err &&  res.render("admin", {cards: foundCards, users: foundUsers})
+        })
       });
     } 
   })
@@ -182,6 +243,7 @@ app.get("/remove/:item", (req, res) => {
 //weather page
 app.get("/weather", function(req, res){
   let rightData = true
+  console.log(req.body.cityName);
   res.render("weather", {weatherDescription: "", cityName: "", weatherTemp: "", imgUrl: "", rightData: rightData})
 })
 
@@ -189,6 +251,7 @@ app.get("/weather", function(req, res){
 //weather page 
 app.post("/weather", function(req, res){
   const cityName = req.body.cityName;
+  console.log(req.body.cityName);
   const appId = "1c5ed49367b45ab915f439370e43a902"
   const units = "Imperial"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid=" + appId +"&units="+ units
@@ -204,7 +267,7 @@ app.post("/weather", function(req, res){
       if (weatherData.cod == '404') {
         rightData = false
       } else {
-        weatherTemp = weatherData.main.temp
+        weatherTemp = Math.floor(((weatherData.main.temp - 32)*5)/9)
         weatherDescription = weatherData.weather[0].description
         weatherIcon = weatherData.weather[0].icon
   
@@ -218,10 +281,9 @@ app.post("/weather", function(req, res){
 })
 
 //contact page
-app.get("/contact", (req, res) => {
+app.get("/contact", function (req, res) {
   res.render("contact")
 })
-
 
 
 
@@ -230,9 +292,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-
-
-
 
 app.listen(port, () => {
   console.log("Server started on port 3000");
